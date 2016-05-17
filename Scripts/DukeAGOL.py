1fdac84138cb398cd3745cd3baa73f6ce8121faf
jpfay@duke.edu <jpfay@duke.edu>
2016-05-17 14:00:26 -0400

    Adds portalpy functions and disable user

--- Scripts/DukeAGOL.py
+++ Scripts/DukeAGOL.py
@@ -1,21 +1,56 @@
-#https://github.com/Esri/ago-tools
+# DukeAGOL.py
+#
+# Description: A set of functions implementing the agoTools and PortalPy packages
+#  to manage Duke's AGOL accounts
+#
+# AgoTools Source: https://github.com/Esri/ago-tools
+# PortalPy Source: http://resources.arcgis.com/en/help/main/10.2/index.html#/PortalPy_module/017s000000m2000000/
+#
+# May 2016
+# John.Fay@duke.edu
+
 
 # Requires admin role.
 import csv, time
 from agoTools.admin import Admin
+import portalpy
 
+# Code to easy log in (for me)
 logAdmin = "john.fay"
 if not "logPwd" in dir():
-    logPwd = raw_input("Enter password: ")
+    logPwd = raw_input("Enter password for {}: ".format(logAdmin))
 
-agoAdmin = Admin('john.fay',password=logPwd) 
+#Create agoAdmin objects
+agoAdmin = Admin(username=logAdmin,portal="https://dukeuniv.maps.arcgis.com",password=logPwd)
 users = agoAdmin.getUsers()
 roles = agoAdmin.getRoles()
+
+#Create the portalpy portal object
+portal = portalpy.Portal(url="https://dukeuniv.maps.arcgis.com",username=logAdmin,password=logPwd)
 
 #Make a dictionary of the roles so we can convert custom roles from their ID to their associated name.
 roleLookup = {}
 for role in roles:
     roleLookup[role["id"]] = role["name"]
+
+def disableUser(username):
+    doIt = raw_input("Disable "+username+"?")
+    if doIt == "y":
+        print "removing "+ username
+        portal.disable_user(username)
+
+def removeNullUsers():
+    '''Disables accounts that have never logged in and with no items'''
+    for user in users:
+        #Get data user last logged in
+        lastLogin = time.strftime("%Y-%m-%d",time.gmtime(user['lastLogin']/1000))
+        loginYear = time.gmtime(user['lastLogin']/1000).tm_year
+        #If the user never logged in (year will be before 1971)...
+        if loginYear < 1971:
+            #Get items for user
+            items = agoAdmin.AGOLCatalog('owner:{}'.format(user['username']))
+            if len(items) == 0:
+                disableUser(user['username'])
 
 def writeUserCSV():
     '''Writes user and user info to CSV file'''
@@ -58,4 +93,4 @@ def moveItems(userFrom,userTo):
     agoAdmin.addUser2ToAllUser1Groups(userFrom, userTo)
     return
 
-moveItems('env761_jpfay','jpfay_dukeuniv')
+# moveItems('env761_jpfay','jpfay_dukeuniv')
